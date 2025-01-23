6e3509dd72113bbf787c36a5b7c89b2ff2e9da6e
LunaMellow <nore0404@gmail.com>
2025-01-23 13:31:35 +0100

    Cleanup Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,5 @@
 module Main (main) where
-
-isLeapYear :: Int -> Bool
-isLeapYear year
-    | divisible 400 == 0 = True
-    | divisible 100 == 0 = False
-    | divisible 4   == 0 = True
-    | otherwise          = False
-    where divisible n = year `mod` n
-
+    
 main :: IO ()
 main = do
-    putStrLn "\n-------------------------------------"
-    putStrLn "\n(0) Quit program\n(1) Check leap year"
-    quit <- getLine
-    let quitInt = read quit :: Int
-    if quitInt == 0
-    then putStrLn "\nExiting program...\n"
-    else do
-        putStrLn "\nEnter year to check if it's a leap year:"
-        yearInput <- getLine
-        let year = read yearInput :: Int
-        if isLeapYear year
-            then putStrLn "\nIt's a leap year!" >> main
-            else putStrLn "\nNot a leap year." >> main
+    putStrLn "\nMain Program"
